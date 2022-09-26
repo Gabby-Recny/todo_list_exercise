6f7394249a1ad307954abe2f1b645391a2844ce2
Gabby Recny <gabriellerecny@rightpoint.com>
2022-09-26 16:17:53 -0400

    Display selected number on thank you message

--- Downloads/interactive-rating-component-main/main.js
+++ Downloads/interactive-rating-component-main/main.js
@@ -1,6 +1,5 @@
 //Add click styling to selected radio button
 //Only one should be submitted
-//On submit, hide survey and show thank you message
 // Inside thank you message, show rating
 
 //Query selectors
@@ -9,19 +8,31 @@ const radioButtons = document.querySelectorAll('input[name=radio-rating]');
 const form = document.querySelector('form');
 const submitBtn = document.querySelector('button');
 const thankYouMsg = document.querySelector('.thank-you-msg');
+const selectedRating = document.getElementById('selectedRating');
+
+const handleSelection = (event) => {
+    event.target.classList.add('selected')
+    selectedRating.innerText = event.target.value
+
+}
 
 const handleSubmission = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     form.classList.add('hidden');
     thankYouMsg.classList.remove('hidden');
 }
 
 
-
-
-
-
 // Event listeners
+window.addEventListener('load', (e) => {
+    radioButtons.forEach(input => {
+        input.addEventListener('click', (e) => {
+            e.target.classList.remove('selected')
+            handleSelection(e)
+        })
+    })
+})
+
 submitBtn.addEventListener('click', (e) => {
     handleSubmission(e)
 })
