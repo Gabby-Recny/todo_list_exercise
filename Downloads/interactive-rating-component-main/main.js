69cec30983998b6bf3ec2291a2bdd22e03433edd
Gabby Recny <gabriellerecny@rightpoint.com>
2022-09-26 16:37:16 -0400

    Add selected styling to inputs

--- Downloads/interactive-rating-component-main/main.js
+++ Downloads/interactive-rating-component-main/main.js
@@ -1,8 +1,3 @@
-//Add click styling to selected radio button
-//Only one should be submitted
-// Inside thank you message, show rating
-
-//Query selectors
 const ratings = document.querySelectorAll('.rating');
 const radioButtons = document.querySelectorAll('input[name=radio-rating]');
 const form = document.querySelector('form');
@@ -10,29 +5,27 @@ const submitBtn = document.querySelector('button');
 const thankYouMsg = document.querySelector('.thank-you-msg');
 const selectedRating = document.getElementById('selectedRating');
 
-const handleSelection = (event) => {
-    event.target.classList.add('selected')
-    selectedRating.innerText = event.target.value
-
-}
-
 const handleSubmission = (event) => {
     event.preventDefault();
     form.classList.add('hidden');
     thankYouMsg.classList.remove('hidden');
 }
 
+const removeStyling = () => {
+    ratings.forEach(rating => {
+        rating.classList.remove('selected')
+    })
+}
 
-// Event listeners
-window.addEventListener('load', (e) => {
-    radioButtons.forEach(input => {
-        input.addEventListener('click', (e) => {
-            e.target.classList.remove('selected')
-            handleSelection(e)
+const handleSelection = () => {
+    radioButtons.forEach(btn => {
+        btn.addEventListener('click', ()=> {
+            radioButtons.forEach(btn => btn.classList.remove('selected'))
+            btn.classList.add('selected')
+            selectedRating.innerText = btn.value
         })
     })
-})
+}
 
-submitBtn.addEventListener('click', (e) => {
-    handleSubmission(e)
-})
+window.addEventListener('load', (e) => { handleSelection(e) })
+submitBtn.addEventListener('click', handleSubmission)
